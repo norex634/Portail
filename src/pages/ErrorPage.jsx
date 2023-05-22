30f871c28c1a7405b6ba3d8b69c91d0d08eeb8f5
vandenessef <vandenessef@CLASSES.LOCAL>
2023-05-22 11:08:55 +0200

    footer

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,15 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 
 const ErrorPage = (props) => {
     return ( 
         <>
             <div className="slide" id="error">
+                <Header />
                 <h1>La société n'existe plus(pas).</h1>
             </div>
+            <Footer />
         </>
      );
 }
